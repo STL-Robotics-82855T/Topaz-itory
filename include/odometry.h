96a69be9a994936b5a00cc2e27f5276d924fa302
DO-Ui <68296986+DO-Ui@users.noreply.github.com>
2023-07-29 10:38:15 -0400

    Added comments, tidied up code & added comments

--- include/odometry.h
+++ include/odometry.h
@@ -7,6 +7,7 @@ class odometry {
     public:
         float current_angle_deg;
         float current_angle_rad;
+        float current_heading_deg;
         float inches_per_second;
         float inches_per_rotation;
         pair<float, float> absolute_position = {0.0, 0.0};
@@ -23,7 +24,7 @@ class odometry {
             inches_per_rotation = wheel_size * PI;
         }
 
-        // Threads
+        /// @brief Gets the current angle of the robot, updates every 5ms
         void get_current_angle() { // odom rotation thread
 
             while (true) {
@@ -33,7 +34,6 @@ class odometry {
                 current_angle_deg = (imu1 + imu2) / 2;
                 // cut off to 3 decimal places
                 current_angle_deg = floorf(current_angle_deg * 1000) / 1000;
-
                 current_angle_rad = current_angle_deg * (PI / 180.0);
 
                 // master.print(0, 0, "IMU 1: %.2f\n", imu_sensor1.get_rotation()*(365.0/360.0)); // Offset factor due to some weirdness with the IMU
@@ -43,6 +43,8 @@ class odometry {
                 Task::delay(5);
             }
         }
+
+        /// @brief Gets the cyurrent direction of the robot, updates every 5ms
         void get_current_position() {
             // Positions are measured in number of motor rotations (not degrees)
             float prev_left_pos = 0;
@@ -55,6 +57,8 @@ class odometry {
             // Measured in rad
             float previous_angle = 0;
             float angle_delta;
+            float offset_theta = 0;
+            float offset_radius = 0;
 
             // Distances are measured in inches
             float left_side_distance_delta = 0;
@@ -72,7 +76,7 @@ class odometry {
                 right_side_distance_delta = (right_pos - prev_right_pos) * inches_per_rotation;
 
                 // ΔS
-                horizontal_distance_delta = (horizontal_pos - prev_horizontal_pos) * inches_per_rotation;   
+                horizontal_distance_delta = (horizontal_pos - prev_horizontal_pos) * inches_per_rotation;
 
                 // Δθ
                 angle_delta = smart_radian_diff(current_angle_rad, previous_angle);
@@ -82,18 +86,18 @@ class odometry {
                     local_offset.first = horizontal_distance_delta;
                     local_offset.second = right_side_distance_delta;
                 } else { // Arc
-                    float x_step_1 = horizontal_distance_delta/angle_delta + back_offset;
-                    float y_step_1 = right_side_distance_delta/angle_delta + right_offset;
-                    local_offset.first = x_step_1 * (2 * sin(angle_delta / 2));
-                    local_offset.second = y_step_1 * (2 * sin(angle_delta / 2));
+                    local_offset.first = (horizontal_distance_delta/angle_delta + back_offset) * (2 * sin(angle_delta / 2));
+                    local_offset.second = (right_side_distance_delta/angle_delta + right_offset) * (2 * sin(angle_delta / 2));
                 }
 
-                float offset_theta = atan2f(local_offset.second, local_offset.first);
-                float offset_radius = sqrt(pow(local_offset.first, 2) + pow(local_offset.second, 2));
+                // Rotate offset vector to match robot's current heading
+                offset_theta = atan2f(local_offset.second, local_offset.first);
+                offset_radius = sqrt(pow(local_offset.first, 2) + pow(local_offset.second, 2));
                 offset_theta = offset_theta - current_angle_rad + angle_delta / 2;
                 local_offset.first = offset_radius * cos(offset_theta);
                 local_offset.second = offset_radius * sin(offset_theta);
 
+                // Update absolute position
                 absolute_position.first += local_offset.first;
                 absolute_position.second += local_offset.second;
 
@@ -109,19 +113,25 @@ class odometry {
 
     private:
         // Offsets are measured in inches
-        float left_offset;
-        float right_offset;
-        float back_offset;
+        float left_offset; // Inches measured from the center of the robot
+        float right_offset; // Inches measured from the center of the robot
+        float back_offset; // Inches measured from the center of the robot
         float wheel_size; // Diameter measured in inches
         float wheel_rpm;
         float motor_rpm;
         pair<float, float> local_offset = {0.0, 0.0}; // Measured in inches
 
+
+        /// @brief Calculates the difference between two angles in radians, always returns the smallest difference
+        /// @param rad1 
+        /// @param rad2 
+        /// @return Difference between the two angles in radians
         float smart_radian_diff(float rad1, float rad2) {
             float diff = rad1 - rad2;
-            if (diff > PI) {
+            if (diff >= PI) {
                 diff -= 2 * PI;
-            } else if (diff < -PI) {
+            }
+            if (diff < -PI) {
                 diff += 2 * PI;
             }
             return diff;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include "main.h"
 #include "devices.h" // Defines motors, controller, sensors and helper functions
-
+#include "movement.h" // Defines movement class
 #include "odometry.h" // Defines odometry class
-
-
-float P = 1;
-float D = 0.5;
 
 // Left offset: 5.8"
 // Right offset: 5.8"
@@ -15,9 +11,6 @@ float D = 0.5;
 // Motor RPM: 600
 // Tracking center: (7.25", 7")
 odometry odom(5.8, 5.8, 0.5, 3.25, 360, 600, pair<float, float>(7.25, 7));
-
-// float current_angle = 0;
-// float previous_angle = 0;
 
 
 void initialize() {
@@ -36,71 +29,13 @@ void initialize() {
 	Task odom_angle_task([] { odom.get_current_angle(); });
 	Task odom_position_task([] { odom.get_current_position(); });
 
+	turn_to_angle(90, true);
 }
 
 
 void disabled() {}
 
 void competition_initialize() {}
-
-
-void turn_right_to_angle(float degrees) {
-
-	// TODO: Implement Odom (or use IMU)
-
-}
-
-void drive_forward(int desired_rotations) {
-	
-	int correct_cycles = 0;
-
-	float left_error = 0;
-	float right_error = 0;
-	float previous_left_error = 0;
-	float previous_right_error = 0;
-	
-	float left_derivative = 0;
-	float right_derivative = 0;
-
-	while (correct_cycles < 100) {
-
-		vector<double> left_positions = left.get_positions();
-		float left_position = (left_positions[0] + left_positions[1] + left_positions[2]) / 3;
-		vector<double> right_positions = right.get_positions();
-		float right_position = (right_positions[0] + right_positions[1] + right_positions[2]) / 3;
-
-
-		lcd::print(0, "Left Position: %f", left_position);
-		lcd::print(1, "Right Position: %f", right_position);
-
-		left_error = desired_rotations - left_position;
-		right_error = desired_rotations - right_position;
-
-		left_derivative = left_error - previous_left_error;
-		right_derivative = right_error - previous_right_error;
-
-		previous_left_error = left_error;
-		previous_right_error = right_error;
-
-		lcd::print(2, "Left Error: %f", left_error);
-		lcd::print(3, "Right Error: %f", right_error);
-
-		if (left_error < 0.08 && right_error < 0.08) {
-			correct_cycles++;
-		} else {
-			correct_cycles = 0;
-		}
-
-		float left_power = left_error * P * 100 + left_derivative * D * 100;
-		float right_power = right_error * P * 100 + right_derivative * D * 100;
-
-		left.move(left_power);
-		right.move(right_power);
-
-		delay(5);
-
-	}
-}
 
 void autonomous() {
 
@@ -127,10 +62,6 @@ void autonomous() {
 }
 
 void opcontrol() {
-
-
-
-	// drive_forward(5);
 
 	int index = 0;
 
