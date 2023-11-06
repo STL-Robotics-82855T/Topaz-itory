5935dcac4d7fcf4d39eb5985b90984222d723023
DO-Ui <68296986+DO-Ui@users.noreply.github.com>
2023-11-05 23:19:00 -0500

    Temporarily remove auton code

--- src/main.cpp
+++ src/main.cpp
@@ -110,27 +110,27 @@ void autonomous() {
 
 	// time_t start_time = std::time(nullptr); // Get the current time in seconds
 
-	int path_size = path.size();
+	// int path_size = path.size();
 
-	for (int index = 0; index < path_size; index++) {
-		squiggles::ProfilePoint point = path[index];
-		vector<double> wheel_velocities = point.wheel_velocities;
-		double left_velocity = wheel_velocities[0];
-		double right_velocity = wheel_velocities[1];
-		double last_time = point.time;
-		double next_time = path[std::min(index + 1, path_size)].time;
-		double wait_time = next_time - last_time;
+	// for (int index = 0; index < path_size; index++) {
+	// 	squiggles::ProfilePoint point = path[index];
+	// 	vector<double> wheel_velocities = point.wheel_velocities;
+	// 	double left_velocity = wheel_velocities[0];
+	// 	double right_velocity = wheel_velocities[1];
+	// 	double last_time = point.time;
+	// 	double next_time = path[std::min(index + 1, path_size)].time;
+	// 	double wait_time = next_time - last_time;
 
-		// Hopefully this works :O
-		double left_power = left_velocity / MAX_VELOCITY * 127;
-		double right_power = right_velocity / MAX_VELOCITY * 127;
+	// 	// Hopefully this works :O
+	// 	double left_power = left_velocity / MAX_VELOCITY * 127;
+	// 	double right_power = right_velocity / MAX_VELOCITY * 127;
 
-		left.move(left_power);
-		right.move(right_power);
+	// 	left.move(left_power);
+	// 	right.move(right_power);
 
-		delay(wait_time * 1000);
+	// 	delay(wait_time * 1000);
 
-	}
+	// }
 
 }
 
